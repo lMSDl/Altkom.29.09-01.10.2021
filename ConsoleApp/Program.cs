e15299fe2e0337c6a19df7cad09d5543236b266d
Paweł Łęczycki <pawel@msd.info.pl>
2021-09-29 10:38:13 +0200

    Aplikacja konsolowa

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using Models;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person { FirstName = "Ewa", LastName = "Ewowska" };
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"Hello {person.LastName} {person.FirstName}!");
+            }
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Models
+{
+    public class Person
+    {
+	public string FirstName {get; set;}
+	public string LastName {get; set;}
+    }
+}
